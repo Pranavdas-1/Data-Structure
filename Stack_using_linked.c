51db0175ad9157a590651c99ddc988d1e1d95733
Pranav Das <138027582+Pranavdas-1@users.noreply.github.com>
2023-12-07 09:53:59 +0000

    Update Stack_using_linked.c

--- Stack_using_linked.c
+++ Stack_using_linked.c
@@ -1,66 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node {
-    int info;
-    struct node *ptr;
+struct node{
+	int info;
+	struct node *ptr;
 }*top,*top1,*temp;
-
 int count = 0;
-void push(int data) {
-     if (top == NULL)
-    {
-        top =(struct node *)malloc(1*sizeof(struct node));
-        top->ptr = NULL;
-        top->info = data;
-    }
-    else
-    {
-        temp =(struct node *)malloc(1*sizeof(struct node));
-        temp->ptr = top;
-        temp->info = data;
-        top = temp;
-    }
-    count++;
-    printf("Node is Inserted\n\n");
+void push(int data){
+	if (top == NULL){
+		top =(struct node *)malloc(1*sizeof(struct node));
+		top->ptr = NULL;
+		top->info = data;
+	}
+	else{
+		temp =(struct node *)malloc(1*sizeof(struct node));
+		temp->ptr = top;
+		temp->info = data;
+		top = temp;
+	}
+	count++;
+	printf("Node is Inserted\n\n");
 }
-
-int pop() {
-     top1 = top;
- 
-    if (top1 == NULL)
-    {
-        printf("\nStack Underflow\n");
-        return -1;
-    }
-    else
-        top1 = top1->ptr;
-    int popped = top->info;
-    free(top);
-    top = top1;
-    count--;
-    return popped;
+int pop(){
+	top1 = top;
+	if (top1 == NULL){
+		printf("\nStack Underflow\n");
+		return -1;
+	}
+	else
+		top1 = top1->ptr;
+	int popped = top->info;
+	free(top);
+	top = top1;
+	count--;
+	return popped;
 }
-
-void display() {
-    // Display the elements of the stack
-    top1 = top;
- 
-    if (top1 == NULL)
-    {
-        printf("\nStack Underflow\n");
-        return;
-    }
-    
-    printf("The stack is \n");
-    while (top1 != NULL)
-    {
-        printf("%d--->", top1->info);
-        top1 = top1->ptr;
-    }
-    printf("NULL\n\n");
-
+void display(){
+	top1 = top;
+	if (top1 == NULL){
+		printf("\nStack Underflow\n");
+		return;
+	}
+	printf("The stack is \n");
+	while (top1 != NULL){
+		printf("%d--->", top1->info);
+		top1 = top1->ptr;
+	}
+	printf("NULL\n\n");
 }
-
 int main(){
 	int choice, value;
 	while (1){
